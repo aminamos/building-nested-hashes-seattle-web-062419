c1b0f07b0ce8f3dd30fa2c2408558520f0861b9f
Amin Amos <aminamosahs@gmail.com>
2019-06-19 03:14:20 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      patriarch: {},
-      matriarach: {},
-      hero: {},
-      hero_friends: []
-      }, 
-    :capulet => {
-      patriarch: {},
-      matriarach: {},
-      heroine: {},
-      heroine_friends: []
-      }
-    
-  }
-
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
   
 end
